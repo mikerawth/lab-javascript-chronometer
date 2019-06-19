ba130bdc83d9d00400750c76bad80fc5ae90ea84
Michael Roth <michael.franklin.roth@gmail.com>
2019-06-19 12:59:03 -0400

    finished creating chronometer class object.  Next step is to get main.js filled out.

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,46 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+    this.splitCounter = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      console.log(this);
+    }, 1000);
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  getSeconds() {
+    return this.currentTime % 60
+  }
+  twoDigitsNumber() {
+    let displayMinutes, displaySeconds;
+
+    if (this.getSeconds() < 10) {
+      displaySeconds = `0${this.getSeconds()}`
+    } else {
+      displaySeconds = `${this.getSeconds()}`
+    }
+
+    if (this.getMinutes() < 10) {
+      displayMinutes = `0${this.getMinutes()}`
+    } else {
+      displayMinutes = `${this.getMinutes()}`
+    }
+
+    return `${displayMinutes}:${displaySeconds}`
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {
+    this.splitCounter += 1;
+    return `${this.splitCounter}. ${this.twoDigitsNumber()}`
+  }
 }
